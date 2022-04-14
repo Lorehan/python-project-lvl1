aedaf3c0f5b6abd8a89b0f11287ddf04e7fded55
Maksim Pushkarev <mpush.dev@gmail.com>
2022-04-14 18:14:47 +0800

    fix progression_game.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -4,23 +4,33 @@ import random
 RULE = 'What number is missing in the progression?'
 
 
-def get_question():
-    start_progression = random.randint(1, 50)
-    end_progression = random.randint(100, 150)
-    step = random.randint(-10, 10)
-    if step == 0:
-        step += 1
-    if step > 0:
+def get_progression(initial_term, last_term, common_difference):
+    if common_difference == 0:
+        common_difference += 1
+    if common_difference > 0:
         progression = list(range(
-            start_progression,
-            end_progression,
-            step))[0:10]
+            initial_term,
+            last_term,
+            common_difference))[0:10]
     else:
         progression = list(range(
-            end_progression,
-            start_progression,
-            step))[0:10]
+            last_term,
+            initial_term,
+            common_difference))[0:10]
+    return progression
+
+
+def get_string_progression(progression, term):
+    progression[progression.index(term)] = '..'
+    string_progression = ' '.join(map(str, progression))
+    return string_progression
+
+
+def get_question():
+    initial_term = random.randint(1, 50)
+    last_term = random.randint(100, 150)
+    common_difference = random.randint(-10, 10)
+    progression = get_progression(initial_term, last_term, common_difference)
     correct_answer = random.choice(progression)
-    progression[progression.index(correct_answer)] = '..'
-    question = ' '.join(map(str, progression))
+    question = get_string_progression(progression, correct_answer)
     return question, str(correct_answer)
