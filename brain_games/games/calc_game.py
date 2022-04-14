edd74578cac445e481a5e467bb6f9e15ddb1af37
Maksim Pushkarev <mpush.dev@gmail.com>
2022-04-14 16:09:29 +0800

    fix calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -9,13 +9,11 @@ def get_question():
     operation = random.choice(OPERATIONS)
     first_number = random.randint(1, 50)
     second_number = random.randint(1, 50)
+    question = f'{first_number} {operation} {second_number}'
     if operation == '+':
-        question = f'{first_number} + {second_number}'
         correct_answer = first_number + second_number
     elif operation == '-':
-        question = f'{first_number} - {second_number}'
         correct_answer = first_number - second_number
     else:
-        question = f'{first_number} * {second_number}'
         correct_answer = first_number * second_number
     return question, str(correct_answer)
